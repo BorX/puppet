63fcf0d377f33705aa795f8781d9d7b9431ad35a
Boris Klymko <boris.klymko@gmail.com>
2013-08-08 18:56:47 +0200

    Console packages

---
+++ modules/console/manifests/init.pp
@@ -0,0 +1,7 @@
+class console {
+	package{[ 'git', 'mutt', 'bash-completion', 'lsof', 'less', 'most', 'foremost', 'file', 'vim', 'colordiff', 'pv', 'ccze', 'lwatch', 'fdupes', 'sysstat', 'time', 'iftop', 'atop', 'iotop', 'htop', 'rsync', 'whois', 'deborphan', 'p7zip-full', 'unzip', 'zip', 'byobu', 'apt-show-versions' ]:}
+}
+
+class console::plus {
+	package{[ 'bc', 'dc', 'at', 'nmap', 'ftp', 'telnet', 'w3m', 'pm-utils', 'hdparm' ]:}
+}
