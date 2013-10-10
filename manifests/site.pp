10325e946fa1c7016e331af1369d38a640aee4c5
Boris Klymko <boris.klymko@gmail.com>
2013-10-10 17:39:03 +0200

    Less nodes, more classes

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,11 +26,34 @@ node debian {
 	}
 }
 
-node xen, phpmu, pub, ssh, petunia inherits debian {
-	class{'iptables':} -> class{'sources':} -> class{'locales':} -> class{'exim4':} -> class{'keyboard':} -> class{'users':} -> class{'dotfiles':} -> class{'hosts':} -> class{'ssh':} -> class{'console':} -> class{'ntp':}
+class base {
+	class{'iptables':}
+	-> class{'sources':}
+	-> class{'locales':}
+	-> class{'exim4':}
+	-> class{'keyboard':}
+	-> class{'users':}
+	-> class{'dotfiles':}
+	-> class{'hosts':}
+	-> class{'ssh':}
+	-> class{'console':}
+	-> class{'ntp':}
+}
+
+node xen, phpmu, pub, ssh inherits debian {
+	class{'base':}
+}
+
+node petunia inherits debian {
+	class{'base':}
+	-> class{'security':}
+	-> class{'gnome':}
 }
 
 node test inherits debian {
-	class{'iptables':} -> class{'sources':} -> class{'locales':} -> class{'exim4':} -> class{'keyboard':} -> class{'users':} -> class{'dotfiles':} -> class{'hosts':} -> class{'ssh':} -> class{'console':} -> class{'ntp':} -> class{'security':} -> class{'tests':} -> class{'console::plus':}
+	class{'base':}
+	-> class{'security':}
+	-> class{'tests':}
+	-> class{'console::plus':}
 }
 
