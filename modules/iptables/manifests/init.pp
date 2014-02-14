8473f952d28d15097786777e1e1e18059fa5e32f
Boris Klymko <boris.klymko@gmail.com>
2014-02-14 11:25:56 +0100

    iptables script as a service

--- modules/iptables/manifests/init.pp
+++ modules/iptables/manifests/init.pp
@@ -9,28 +9,37 @@ class iptables {
 		sourceselect => all,
 		recurse => true,
 	}
- 	file {'/etc/iptables/iptables-init':
+	-> file {'/etc/iptables/iptables-init':
 		owner   => root,
 		group   => root,
 		mode    => 700,
 		source  => [ "puppet:///modules/iptables/iptables_$clientcert/iptables-init", 'puppet:///modules/iptables/iptables/iptables-init' ],
- 	}
- 	file {'/etc/iptables/iptables-commands':
+	}
+	-> file {'/etc/iptables/iptables-commands':
 		owner   => root,
 		group   => root,
 		mode    => 700,
 		source  => [ "puppet:///modules/iptables/iptables_$clientcert/iptables-commands", 'puppet:///modules/iptables/iptables/iptables-commands' ],
- 	}
-
-	file {'/etc/rc.local':
+	}
+	-> file {'/etc/init.d/iptables':
 		owner   => root,
 		group   => root,
 		mode    => 755,
-		source  => [ "puppet:///modules/iptables/rc.local_$clientcert", 'puppet:///modules/iptables/rc.local' ],
+		content => '#!/bin/bash
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:
+# Required-Start:    iptables
+# Required-Stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Initialize iptables rules.
+# Description:
+### END INIT INFO
+/etc/iptables/iptables-init $1',
 	}
-
-	exec {'/etc/rc.local':
-		subscribe   => File['/etc/iptables/', '/etc/iptables/iptables-init', '/etc/iptables/iptables-commands', '/etc/rc.local'],
-		refreshonly => true
+	-> file { '/etc/rc2.d/S99iptables':
+		ensure => 'link',
+		target => '/etc/init.d/iptables',
 	}
 }
